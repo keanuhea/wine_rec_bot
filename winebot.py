96f7117b774ddd9d34870224d63db28f5c91ad7b
captainwolverine28 <keanuheaparker@gmail.com>
2023-12-30 16:50:51 -1000

    need to clean up some stuff, want this to be saved

--- winebot.py
+++ winebot.py
@@ -13,19 +13,21 @@ recommend a wine from a food pairing and vice versa.
 *
 ***************************************************************************************/"""
 
-from collections import Counter
+from collections import Counter, OrderedDict
 import numpy as np
 import nltk
 import re
-import sklearn.manifold
 import multiprocessing
 import pandas as pd
 import gensim.models.word2vec as w2v
 import csv
 import json
 
+from gensim.models import Word2Vec
+
+
 #read in the file
-data = pd.read_json(r'/Users/anuheaparker/Desktop/portfolio/wine_rec/winemag-data-130k-v2.json', dtype={
+data = pd.read_json(r'/Users/anuheaparker/Desktop/wine_bot/wine_rec/wine_rec_bot/winemag-data-130k-v2.json', dtype={
     'points': np.int32,
     'price': np.float32,
 })
@@ -34,6 +36,49 @@ data = pd.read_json(r'/Users/anuheaparker/Desktop/portfolio/wine_rec/winemag-dat
 labels = data['variety']
 descriptions = data['description']
 wine_title = data['title']
+
+description_test = descriptions[0:5]
+wine_title_test = wine_title[0:5]
+
+print(description_test)
+print(wine_title_test)
+
+
+tokenized_corpus = [review.lower().split() for review in description_test]
+
+model = Word2Vec(sentences=tokenized_corpus, vector_size=50, window=2, sg=1, min_count=1)
+
+# User input for taste description
+user_description = input("Describe your taste preferences (e.g., dry, full-bodied, fruity): ").lower().split()
+
+# Calculate the average embedding for the user's taste description
+user_embedding = np.mean([model.wv[word] for word in user_description], axis=0)
+
+# Calculate the average embedding for the user's taste description
+valid_words = [word for word in user_description if word in model.wv]
+if not valid_words:
+    print("No valid words found in the vocabulary. Please provide different taste preferences.")
+else:
+    user_embedding = np.mean([model.wv[word] for word in valid_words], axis=0)
+    
+
+
+	# Calculate cosine similarity between user's taste and wine titles
+	similarities = [np.dot(user_embedding, model.wv[taste.lower()]) for taste in description_test]
+    # Check if all similarities are NaN (no valid words found in the vocabulary)
+    if all(np.isnan(similarity) for similarity in similarities):
+        print("No valid words found in the vocabulary. Please provide different taste preferences.")
+    else:
+        recommended_wine_idx = np.nanargmax(similarities)
+
+
+		# Output wine recommendation
+		recommended_wine = wine_title_test[recommended_wine_idx]
+		print(f"We recommend trying: {recommended_wine}")
+
+
+
+
 
 #will count up how many of each type of wine (ex. albarino) is in the dataset
 #varietal_counts = labels.value_counts()
@@ -48,6 +93,11 @@ def food_pairing(food):
 def taste_description(taste):
 	print("this is the description you want to use", taste)
 
+
+
+
+
+"""
 
 #initial conversation with the user
 def user_convo(search_style):
@@ -70,3 +120,5 @@ if __name__ == "__main__":
     search_style = input("How would you like to search for your perfect wine? Please type either 'food pairing' or 'taste description' to start your search process. ").strip()
     user_convo(search_style)
     
+"""
+
